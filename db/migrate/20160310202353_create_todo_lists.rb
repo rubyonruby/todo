b658b11f2c3f32ae4609a2a5814e70a0b1a54126
rubyonruby <appasani.n@gmail.com>
2016-03-14 00:55:21 -0400

    added new to do list

--- db/migrate/20160310202353_create_todo_lists.rb
+++ db/migrate/20160310202353_create_todo_lists.rb
@@ -2,6 +2,7 @@ class CreateTodoLists < ActiveRecord::Migration
   def change
     create_table :todo_lists do |t|
       t.string :title
+      t.string :title 2
       t.text :description
 
       t.timestamps null: false
